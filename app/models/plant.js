518ee26f04e4e8f7cd05aba80a2ac01de7019729
Jbuckley <buckleyjosh3@gmail.com>
2024-03-02 11:30:10 -0500

    adds plant model

---
+++ app/models/plant.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')
+
+const plantSchema = new mongoose.Schema(
+	{
+		name: {
+			type: String,
+			required: true,
+		},
+		description: {
+			type: String,
+			required: true,
+		},
+		owner: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: 'User'
+		},
+	},
+	{
+		timestamps: true,
+	}
+)
+
+module.exports = mongoose.model('Plant', plantSchema)
